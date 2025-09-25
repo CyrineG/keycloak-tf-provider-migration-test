477d89228c19686f305a0c956ebdc5029c0b8425
Cyrine Gamoudi <cgamoudi@tuev-nord.de>
2025-09-25 16:58:18 +0100

    popluate keycloak.yaml

--- infra/main.tf
+++ infra/main.tf
@@ -11,10 +11,6 @@ terraform {
       source  = "hashicorp/helm"
       version = "3.0.2"
     }
-    keycloak = {
-      source  = "local/mrparkers-sorted/keycloak"
-      version = "4.4.0"
-    }
   }
 }
 
@@ -29,9 +25,28 @@ provider "helm" {
 
 
 locals {
-  argo_cd_config = yamldecode(file("./../clusters/kind-local/argocd-applications/templates/argocd.yaml"))
+  config = yamldecode(file("./secrets.yaml"))
+
+  argo_cd_config = yamldecode(replace(replace(replace(
+    file("./../clusters/kind-local/argocd-applications/templates/argocd.yaml"), "{{ .Values.keycloak.dev_realm.clients.argocd.clientId }}", local.config.keycloak.dev_realm.clients.argocd.clientId),
+    "{{ .Values.keycloak.dev_realm.clients.argocd.clientSecret }}", local.config.keycloak.dev_realm.clients.argocd.clientSecret),
+    "{{ .Values.keycloakDevRealmUrl }}", local.config.keycloak_dev_realm_url)
+  )
   argo_cd_chart_version = local.argo_cd_config.spec.source.targetRevision
   argo_cd_values = yamlencode(local.argo_cd_config.spec.source.helm.valuesObject)
+
+
+  project_members = yamldecode(file("./project_members.yaml"))
+
+  # Add a list of rights to project_members. Look up the rights for each assigned role, join the lists and remove duplicates
+  project_members_with_rights = [
+    for m in local.project_members.project_members : merge(
+      m,
+      {
+        rights : length(m.roles) > 0 ? toset(concat([for role in m.roles : local.config.project_member_roles[role].rights]...)) : []
+      })
+  ]
+
 }
 
 # Install ArgoCD via Helm chart
@@ -84,4 +99,14 @@ resource "helm_release" "argo_cd_apps" {
   depends_on = [
     helm_release.argocd,
   ]
+}
+
+module "keycloak" {
+  source = "./keycloak"
+  base_domain_name                  = local.config.base_domain_name
+  admin_credentials                 = local.config.keycloak.admin
+  external_technical_user_passwords = local.config.external_technical_user_passwords
+  dev_realm                         = local.config.keycloak.dev_realm
+  project_members                   = local.project_members_with_rights #
+  access_token_lifespan             = local.config.keycloak.access_token_lifespan
 }
